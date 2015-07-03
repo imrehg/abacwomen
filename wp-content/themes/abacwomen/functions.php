b4d031ce9e6d0b0110878734173d2a54fbe9d7c6
Gergely Imreh <imrehg@gmail.com>
2015-07-03 16:48:19 +0800

    enable extra buttons in editor

--- wp-content/themes/abacwomen/functions.php
+++ wp-content/themes/abacwomen/functions.php
@@ -10,3 +10,18 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+// Enable hidden buttons in the editor
+// based on: http://wptavern.com/how-to-add-subscript-and-superscript-characters-in-wordpress
+// also https://wordpress.org/support/topic/anchor-button
+function enable_more_buttons($buttons) {
+	/**
+	 * Add in core buttons that are disabled by default
+	 */
+	$buttons[] = 'superscript';
+	$buttons[] = 'subscript';
+	$buttons[] = 'anchor';
+
+	return $buttons;
+}
+add_filter('mce_buttons_2', 'enable_more_buttons');
