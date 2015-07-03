4a4a09099ecfd0d4bff6a9f77243af2dabdf9b3b
Gergely Imreh <imrehg@gmail.com>
2015-07-03 16:55:10 +0800

    temporarily shelve editor changes

--- wp-content/themes/abacwomen/functions.php
+++ wp-content/themes/abacwomen/functions.php
@@ -11,24 +11,24 @@ function theme_enqueue_styles() {
 
 }
 
-// External plugin
-function my_mce_external_plugins($plugins) {
-    $plugins['anchor'] = get_template_directory_uri() . '/js/tinymce/plugins/anchor/plugin.min.js';
-    return $plugins;
-}
-add_filter('mce_external_plugins', 'my_mce_external_plugins');
+/* // External plugin */
+/* function my_mce_external_plugins($plugins) { */
+/*     $plugins['anchor'] = get_template_directory_uri() . '/js/tinymce/plugins/anchor/plugin.min.js'; */
+/*     return $plugins; */
+/* } */
+/* add_filter('mce_external_plugins', 'my_mce_external_plugins'); */
 
-// Enable hidden buttons in the editor
-// based on: http://wptavern.com/how-to-add-subscript-and-superscript-characters-in-wordpress
-// also https://wordpress.org/support/topic/anchor-button
-function enable_more_buttons($buttons) {
-	/**
-	 * Add in core buttons that are disabled by default
-	 */
-	$buttons[] = 'superscript';
-	$buttons[] = 'subscript';
-	$buttons[] = 'anchor';
+/* // Enable hidden buttons in the editor */
+/* // based on: http://wptavern.com/how-to-add-subscript-and-superscript-characters-in-wordpress */
+/* // also https://wordpress.org/support/topic/anchor-button */
+/* function enable_more_buttons($buttons) { */
+/* 	/\** */
+/* 	 * Add in core buttons that are disabled by default */
+/* 	 *\/ */
+/* 	$buttons[] = 'superscript'; */
+/* 	$buttons[] = 'subscript'; */
+/* 	$buttons[] = 'anchor'; */
 
-	return $buttons;
-}
-add_filter('mce_buttons_2', 'enable_more_buttons');
+/* 	return $buttons; */
+/* } */
+/* add_filter('mce_buttons_2', 'enable_more_buttons'); */
